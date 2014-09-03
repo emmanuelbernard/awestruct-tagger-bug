6f67854855350d43a676b3666633cc48849b7ffe
Emmanuel Bernard <emmanuel@hibernate.org>
2014-09-03 15:54:24 +0200

    Project showing regeneration of pages when using tagger fail

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# gem install bundler
+# bundle install
+source 'https://rubygems.org'
+gem 'redcarpet'
+gem 'awestruct', '0.5.5'
---
+++ _ext/pipeline.rb
@@ -0,0 +1,12 @@
+require 'bootstrap-sass'
+
+Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::Posts.new '/news'
+  extension Awestruct::Extensions::Tagger.new( :posts, 
+                                               '/news/index', 
+                                               '/news/tags', 
+                                               :per_page=>10 )
+  # extension Awestruct::Extensions::Indexifier.new
+  # Indexifier *must* come before Atomizer
+  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+end
